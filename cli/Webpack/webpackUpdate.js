e8f22679535b026879bcedac05a99d619136ba8f
Jeremy Tenjo <jeremy.tenjo@fcvinteractive.com>
2018-10-24 17:08:19 -0700

    contionals webpck update

--- cli/Webpack/webpackUpdate.js
+++ cli/Webpack/webpackUpdate.js
@@ -1,5 +1,8 @@
 const shell = require('shelljs')
 
 exports.function = (templatePath, params) => {
-  //1. copy dir from template
+  //1. are you sure?
+  //2 Delete current webpacl/webpackCongfig folder
+  //3. copy webpackCongfig folder folder from template
+  // 4 message
 }
