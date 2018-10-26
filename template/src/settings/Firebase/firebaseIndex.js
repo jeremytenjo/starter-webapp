8762ca3b7a9018553a0e927d2c5de9f92d2eb781
tenjojeremy <tenjojeremy@gmail.com>
2018-10-26 08:22:47 -0700

    Update

--- template/src/settings/Firebase/firebaseIndex.js
+++ template/src/settings/Firebase/firebaseIndex.js
@@ -2,11 +2,11 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 let config = {
-  apiKey: 'AIzaSyAiPnRK-OClvUr0Q-tBOR1xMczXme2Kmcw',
-  authDomain: 'app-template-react.firebaseapp.com',
-  databaseURL: 'https://app-template-react.firebaseio.com',
-  projectId: 'app-template-react',
-  storageBucket: 'app-template-react.appspot.com',
-  messagingSenderId: '219115772263'
+  apiKey: 'AIzaSyB1L1inpSmWPh4QTMaivEFD6oLo8RMvKmw',
+  authDomain: 'webapp-jt.firebaseapp.com',
+  databaseURL: 'https://webapp-jt.firebaseio.com',
+  projectId: 'webapp-jt',
+  storageBucket: '',
+  messagingSenderId: '64204415951'
 }
 firebase.initializeApp(config)
