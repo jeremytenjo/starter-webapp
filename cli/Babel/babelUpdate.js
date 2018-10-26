1cc139a3e097cef97e838e9bfbd7f2180ba6e983
tenjojeremy <tenjojeremy@gmail.com>
2018-10-26 08:43:02 -0700

    Update

--- cli/Babel/babelUpdate.js
+++ cli/Babel/babelUpdate.js
@@ -3,13 +3,14 @@ const emoji = require('node-emoji')
 
 exports.index = (templatePath, currentDir) => {
   let emojiSuccess = () => emoji.find('fire').emoji
+  console.log(currentDir)
 
   //1. are you sure?
   //2 Delete current webpacl/webpackCongfig folder
   shell.rm(`-rf`, `${currentDir}/.babelrc`)
 
   //3. copy webpackCongfig folder folder from template
-  shell.cp('-R', `${templatePath}/.babelrc`, `${currentDir}/`)
+  // shell.cp('-R', `${templatePath}/.babelrc`, `${currentDir}/`)
 
   // 4 message
   console.log('Babel Updated! ' + emojiSuccess())
--- template/bash/wacd.bat
+++ template/bash/wacd.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call cd C:\Dev\Personal Projects\TEMPLATES\app-template-react
+call cd C:\Dev\Personal Projects\Webapp\template
