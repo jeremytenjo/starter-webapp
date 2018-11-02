4e83cc4a1b7a2deac2fa2c698cc325d9a7df4f8f
interactve math <jtinteractivewow@gmail.com>
2018-11-02 21:40:08 +0000

    Update template/api/apiIndex.js

--- template/api/apiIndex.js
+++ template/api/apiIndex.js
@@ -1,12 +1,17 @@
 const functions = require('firebase-functions')
 const cors = require('cors')
 const express = require('express')
-var MongoDB = require('./routes/MongoDB/index.js')
+var MongoDB = require('./routes/MongoDB/MDBIndex.js')
+var PushNotifications = require('./routes/PushNotifications/pnIndex.js')
+
 
 const app = express()
 
 app.use(cors({ origin: true }))
 
 app.use('/MongoDB', MongoDB)
+app.use('/PushNotifications', PushNotifications)
+
+
 
 exports.api = functions.https.onRequest(app)
