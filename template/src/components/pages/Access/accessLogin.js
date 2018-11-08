6d33c87d107395c879b4c4be3897d015da08a2a6
interactve math <jtinteractivewow@gmail.com>
2018-11-08 00:22:40 +0000

    Update template/src/components/pages/Access/accessLogin.js

---
+++ template/src/components/pages/Access/accessLogin.js
@@ -0,0 +1,5 @@
+export default (_username, _password) => {
+    const username = _username || false
+    const password = _password || false
+
+}
--- template/src/components/pages/Access/login.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-const Login = () => {
-  const [title, setTitle] = useState('Login')
-
-  return <div>{title}</div>
-}
-
-export default Login
