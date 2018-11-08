7f984fd42fa08377d469be9ee17613f90cb63365
interactve math <jtinteractivewow@gmail.com>
2018-11-08 00:22:24 +0000

    Update template/src/settings/Router/routerIndex.js

--- template/src/settings/Router/routerIndex.js
+++ template/src/settings/Router/routerIndex.js
@@ -1,7 +1,10 @@
+//Vendors
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import HomeRoutes from '../../components/pages/Home/homeRoutes'
+
+//Routes
 import AccessRoutes from '../../components/pages/Access/accessRoutes'
+import HomeRoutes from '../../components/pages/Home/homeRoutes'
 import Page2Routes from '../../components/pages/Page2/page2Routes'
 
 export default () => (
