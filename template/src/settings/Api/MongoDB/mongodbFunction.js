242d3acf34ff0345839c4d3b07ebde7b11a9b792
interactve math <jtinteractivewow@gmail.com>
2018-11-15 20:41:55 +0000

    Update template/src/settings/Api/MongoDB/mongodbFunction.js

---
+++ template/src/settings/Api/MongoDB/mongodbFunction.js
@@ -0,0 +1,31 @@
+export default (data) => {
+  const userId = data.userId || false 
+  const collection = data.collection || false
+  const fnName = data.fnName || false  
+  const payload = data.payload || false
+
+    if (userId, collection, fnName, payload) {
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const url = `${window.apiURL}MongoDB/${collection}/${fnName}`
+      const dbData = await fetch(url, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ userId, payload })
+      })
+      const dbDataJson = await dbData.json()
+
+      resolve(dbDataJson)
+    } catch (error) {
+      throw new Error(error)
+    }
+  })
+    } else {
+        throw new Error(`Missing params ${userId, collection, fnName, payload}`)
+    }
+
+}
