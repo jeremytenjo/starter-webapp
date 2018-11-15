38b5854f1e73a3195ffcde5c4a13f6dbc00340a9
interactve math <jtinteractivewow@gmail.com>
2018-11-15 23:38:39 +0000

    Update template/src/settings/Api/MongoDB/mongodbFunction.js

--- template/src/settings/Api/MongoDB/mongodbFunction.js
+++ template/src/settings/Api/MongoDB/mongodbFunction.js
@@ -4,22 +4,21 @@ export default (data) => {
   const fnName = data.fnName || false  
   const payload = data.payload || false
 
-    if (userId && collection && fnName && payload) {
-
-  return new Promise(async (resolve, reject) => {
-    try {
-      const url = `${window.apiURL}MongoDB/${collection}/${fnName}`
-      const dbData = await fetch(url, {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({ userId, payload })
-      })
-      const dbDataJson = await dbData.json()
-
-      resolve(dbDataJson)
+  if (userId && collection && fnName && payload) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const url = `${window.apiURL}MongoDB/${collection}/${fnName}`
+        const dbData = await fetch(url, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify({ userId, payload })
+          })
+          const dbDataJson = await dbData.json()
+          
+          resolve(dbDataJson)
     } catch (error) {      
       reject(error)
     }
