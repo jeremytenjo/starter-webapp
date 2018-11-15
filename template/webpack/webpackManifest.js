bcd7993f5affb3f86062cefd27a6889a287a86dc
interactve math <jtinteractivewow@gmail.com>
2018-11-15 19:46:20 +0000

    Update template/webpack/webpackManifest.js

--- template/webpack/webpackManifest.js
+++ template/webpack/webpackManifest.js
@@ -3,9 +3,11 @@ const localIp = require('get-my-local-ip')
 exports.manifest = {
   port: 3001,
   host: localIp.address,
-  project_name: 'Webapp',
-  project_short_name: 'Webapp',
-  project_desc: 'Webapp Template',
+  projectInfo: {
+    name: 'Webapp',
+    shortName: 'Webapp',
+    description: 'Webapp Template'
+  },  
   theme: {
     colors: {
       primary: '#DAD2F0',
