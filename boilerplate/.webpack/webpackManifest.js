a63a0d39cb1d8a124271dc0b8921daa3c6483e5f
Jeremy <tenjojeremy@gmail.com>
2019-02-17 15:17:18 -0800

    updated

--- boilerplate/.webpack/webpackManifest.js
+++ boilerplate/.webpack/webpackManifest.js
@@ -1,7 +1,7 @@
 const localIp = require('get-my-local-ip')
 
 // Theme Manifest
-const ThemeManifestModule = require('../src/theme/theme.manifest')
+const ThemeManifestModule = require('../src/Theme/Theme.manifest')
 const { ThemeManifest: theme } = ThemeManifestModule
 
 // logo
