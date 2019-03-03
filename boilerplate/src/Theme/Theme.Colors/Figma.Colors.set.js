046c4a9213b08ea02fe573a55ba1d668063fc1b7
Jeremy <tenjojeremy@gmail.com>
2019-03-03 12:25:44 -0800

    .

--- boilerplate/src/Theme/Theme.Colors/Figma.Colors.set.js
+++ boilerplate/src/Theme/Theme.Colors/Figma.Colors.set.js
@@ -2,9 +2,11 @@ export default (colorSet) => {
   let colorKey = ''
   let colorValue = ''
   let colorCollection = []
+  let colorUppercase = ''
 
   for (const color in colorSet) {
-    colorKey = `--color-${color}`
+    colorUppercase = color[0].toUpperCase() + color.slice(1)
+    colorKey = `--color-${colorUppercase}`
     colorKey = colorKey.replace(/ /g, '')
     colorValue = colorSet[color]
 
