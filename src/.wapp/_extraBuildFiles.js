bd26bc52e04b22865598e2f42673d4bbb4aa3003
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-16 07:57:43 -0500

    removed wqapp folder

--- src/.wapp/_extraBuildFiles.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  {
-    from: 'C:\\Dev\\Personal_Projects\\other\\starter-webapp/src/fonts',
-    to: 'C:\\Dev\\Personal_Projects\\other\\starter-webapp/build/fonts',
-  },
-]
--- src/.wapp/_firebase/index.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-
-import enablePerfMonitoring from '@tenjojeremy/web-toolkit/analytics/firebase'
-
-firebase.initializeApp({})
-
-enablePerfMonitoring(firebase)
