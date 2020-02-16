6f4935d84ef3045b8ae284794acc27cc7cbf7266
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-16 07:58:59 -0500

    s

--- src/Api/api.setUrl.js
+++
@@ -1,7 +0,0 @@
-const setApiUrl = () => {
-  process.env.NODE_ENV === 'development'
-    ? (window.apiURL = 'http://localhost:5000/movies-series-guide/us-central1/search')
-    : (window.apiURL = 'https://us-central1-movies-series-guide.cloudfunctions.net/api/')
-}
-
-export default setApiUrl
