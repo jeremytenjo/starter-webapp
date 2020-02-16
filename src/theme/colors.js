f7e9c5a80eba177b4d163b82a1e8bc09d78ca78e
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-16 08:36:44 -0500

    coor

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,11 +1,11 @@
-exports.colors = {
+module.exports = {
   primary: '#24c3a9',
   primaryDarker: '#005a34',
-  primaryBackground: '#aaffdb',
+  primaryLighter: '#aaffdb',
 
   secondary: '#ada3ff',
   secondaryDarker: '#6700ca',
-  secondaryBackground: '#ebe8ff',
+  secondaryLighter: '#ebe8ff',
 
   disabledForeground: '#828282',
   disabledBackground: '#f2f2f2',
@@ -19,15 +19,16 @@ exports.colors = {
   white: '#fff',
   whiteOpaque: 'rgba(255, 255, 255, 0.8)',
 
-  black: '#212121',
+  black: '#06070B',
   blackBackground: 'rgba(0, 0, 0, 0.1)',
   blackOpaque: 'rgba(33, 33, 33, 0.8)',
 
-  background: '#ffffff',
+  background: '#06070B',
 
   success: '#48FE93',
   successForeground: '#005a34',
 
+  red: '#FE4848',
   error: '#FE4848',
   errorForeground: 'white',
 }
