4b1c4d6472070bcab16999c1672b6f5b54024182
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-16 08:24:04 -0500

    pages hoem route dont redirect to movies

--- src/pages/home/routes.js
+++ src/pages/home/routes.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from './'
 
 export default () => (
   <Switch>
-    <Route exact path='/' render={() => <Redirect to='/movies' />} />
-    <Route path='/movies' component={Home} />
+    <Route path='/' component={Home} />
   </Switch>
 )
