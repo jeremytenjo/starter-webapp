0fbbbb3f4c477fe81695a98f9143168e8eaf80f5
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-09 13:28:17 -0500

    eslint

--- src/firebase/functions/src/index.ts
+++ src/firebase/functions/src/index.ts
@@ -2,7 +2,8 @@ import * as functions from 'firebase-functions'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const helloWorld = functions.https.onCall((data, context) => {
   return {
     hello: 'hello my name is',
