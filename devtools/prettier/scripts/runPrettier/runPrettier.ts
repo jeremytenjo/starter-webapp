f360f20da1161dbbf42d769c0c37ab907bb4a171
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-09 10:18:27 -0500

    fixed prettier

--- devtools/prettier/scripts/runPrettier/runPrettier.ts
+++ devtools/prettier/scripts/runPrettier/runPrettier.ts
@@ -1,4 +1,4 @@
-import shell from '../../../nodeUtils/shell.js'
+import shell from '../../../utils/node/shell.js'
 ;(async function runPrettier() {
   shell(
     `prettier \"**/*.{js,json,ts,tsx,jsx}\" --write --config ./devtools/prettier/config.json --ignore-path ./devtools/prettier/.prettierignore`,
