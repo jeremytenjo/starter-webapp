1349798f70eb47a03063822e7c82eb7530d0e051
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-13 09:43:27 -0500

    storybook

---
+++ src/pages/HomePage/stories/docs.tsx
@@ -0,0 +1,18 @@
+import {
+  Title,
+  Description,
+  Primary,
+  ArgsTable,
+  PRIMARY_STORY,
+} from '@storybook/addon-docs'
+
+export default function Docs() {
+  return (
+    <>
+      <Title />
+      <Description>This is the home page</Description>
+      <Primary />
+      <ArgsTable story={PRIMARY_STORY} />
+    </>
+  )
+}
---
+++ src/pages/HomePage/stories/sb.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta } from '@storybook/react'
+
+import HomePage from '../HomePage'
+
+import Docs from './docs'
+
+export default {
+  title: 'Pages/Home Page',
+  component: HomePage,
+  args: {},
+  // https://storybook.js.org/docs/react/writing-docs/docs-page#remixing-docspage-using-doc-blocks
+  parameters: {
+    docs: {
+      page: () => <Docs />,
+    },
+  },
+} as ComponentMeta<typeof HomePage>
+
+const Template = (args) => {
+  return (
+    <>
+      <HomePage {...args} />
+    </>
+  )
+}
+
+export const Example = Template.bind({})
