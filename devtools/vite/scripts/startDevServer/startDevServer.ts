04e8ee421bbbfae897f39084c728f0b759b1a84b
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-13 17:41:41 -0500

    dev working

--- devtools/vite/scripts/startDevServer/startDevServer.ts
+++ devtools/vite/scripts/startDevServer/startDevServer.ts
@@ -1,8 +1,5 @@
 import shell from '../../../utils/node/shell.cjs'
-import appConfig from '../../../../app.config.js'
 
 export default async function startDevServer() {
-  shell(
-    `vite --config ./devtools/vite/config.ts --host --port ${appConfig.server.local.port}`,
-  )
+  shell(`vite --config ./devtools/vite/config.ts --host --port 3001`)
 }
