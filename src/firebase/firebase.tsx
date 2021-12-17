7a7b08bea7b224a5d5ba19df9955c6bc9f852e6d
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-17 09:50:39 -0500

    emulator

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { initializeApp } from 'firebase/app'
 import { FirebaseProvider } from '@useweb/use-firebase'
-// import initializeFirebaseEmulator from '@useweb/start-firebase-emulator'
 // import { getFirestore } from 'firebase/firestore'
 // import { getAuth } from 'firebase/auth'
 import { getFunctions } from 'firebase/functions'
@@ -13,8 +12,6 @@ const firebaseApp = initializeApp(firebaseConfig)
 // const db = getFirestore()
 const functions = getFunctions()
 // const messaging = getMessaging()
-
-// initializeFirebaseEmulator({ auth, db, enable: process.env.NODE_ENV === 'development' })
 
 export default function Firebase({ children }) {
   return (
