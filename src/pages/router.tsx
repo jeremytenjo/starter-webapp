e88b2039efdccfe4d798b084c21bad76f6d5de66
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-17 16:36:53 -0500

    ShellLayout

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import React, { Suspense } from 'react'
 
+import ShellLayout from './shell.layout'
 import HomeRoutes from './HomePage/routes'
 
 export function Routes() {
   return (
-    <>
+    <ShellLayout>
       <Suspense fallback={null}>
         <HomeRoutes />
       </Suspense>
-    </>
+    </ShellLayout>
   )
 }
 
