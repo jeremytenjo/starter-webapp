c9301962cc858fe4295934e4c02e8a53f00aa35b
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-26 15:46:04 -0500

    keepScreenAwake

--- src/lib/utils/dom/keepScreenAwakeOnDev/keepScreenAwakeOnDev.js
+++ src/lib/utils/dom/keepScreenAwakeOnDev/keepScreenAwakeOnDev.js
@@ -1,17 +1,19 @@
 const keepScreenAwake = async () => {
-  try {
-    let wakeLock = await navigator.wakeLock.request('screen')
+  if ('wakeLock' in navigator) {
+    try {
+      let wakeLock = await navigator.wakeLock.request('screen')
 
-    document.addEventListener('visibilitychange', async () => {
-      if (wakeLock !== null && document.visibilityState === 'visible') {
-        wakeLock = await navigator.wakeLock.request('screen')
-      }
-    })
-  } catch (err) {
-    console.log(err.name, err.message)
+      document.addEventListener('visibilitychange', async () => {
+        if (wakeLock !== null && document.visibilityState === 'visible') {
+          wakeLock = await navigator.wakeLock.request('screen')
+        }
+      })
+    } catch (err) {
+      console.log(err.name, err.message)
+    }
   }
 }
 
-if (process.env.NODE_ENV === 'development' && 'wakeLock' in navigator) {
+if (process.env.NODE_ENV === 'development') {
   keepScreenAwake()
 }
