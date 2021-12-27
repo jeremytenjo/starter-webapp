b4feca4a4207e2373b7c2eeb0a54884c4bc08339
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-27 08:51:44 -0500

    udpate pull to refersh

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { UseInstallPromptProvider } from '@useweb/use-install-prompt'
 
-import '@useweb/use-pull-to-refresh/public/styles.css'
 import './lib/utils/dom/keepScreenAwakeOnDev/keepScreenAwakeOnDev'
 import Firebase from './firebase/firebase'
 import Router from './pages/router'
