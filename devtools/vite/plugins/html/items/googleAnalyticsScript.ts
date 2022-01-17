8253185ade37bbc015da06127e1c5bb08ea93726
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-17 15:41:56 -0500

    debugmode

--- devtools/vite/plugins/html/items/googleAnalyticsScript.ts
+++ devtools/vite/plugins/html/items/googleAnalyticsScript.ts
@@ -1,15 +1,19 @@
 import { PayloadTypes } from '../../../vite'
 
-export default function googleAnalyticsScript({ appConfig }: PayloadTypes) {
+export default function googleAnalyticsScript({ appConfig, isDevMode }: PayloadTypes) {
   return appConfig?.analytics?.google?.measurementId
     ? `
      <!-- Global site tag (gtag.js) - Google Analytics -->
-     <script async src="https://www.googletagmanager.com/gtag/js?id=${appConfig.analytics.google.measurementId}"></script>
+     <script async src="https://www.googletagmanager.com/gtag/js?id=${
+       appConfig.analytics.google.measurementId
+     }"></script>
      <script>
        window.dataLayer = window.dataLayer || [];
        function gtag(){dataLayer.push(arguments);}
        gtag('js', new Date());  
-       gtag('config', '${appConfig.analytics.google.measurementId}');
+       gtag('config', '${appConfig.analytics.google.measurementId}', { 'debug_mode': ${
+        isDevMode ? 'true' : 'false'
+      } });
      </script>`
     : ''
 }
