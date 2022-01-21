e1671e197fec4c2cc4c7c7d389aa799d213930f8
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-21 14:06:18 -0500

    fixed vite pwa plugin

--- devtools/vite/plugins/pwa/pwa.ts
+++ devtools/vite/plugins/pwa/pwa.ts
@@ -7,13 +7,11 @@ import getWorkbox from './workbox/workbox'
 // import getIncludeAssets from './includeAssets/includeAssets'
 
 // https://vite-plugin-pwa.netlify.app/guide/generate.html
-export default function vitePWAPlugin({ appConfig, isProdMode }: PayloadTypes) {
-  return isProdMode
-    ? VitePWA({
-        // includeAssets: getIncludeAssets(),
-        manifest: getManifest({ manifestJson: appConfig.manifestJson }),
-        strategies: 'generateSW',
-        workbox: getWorkbox(),
-      })
-    : () => null
+export default function vitePWAPlugin({ appConfig }: PayloadTypes) {
+  // https://vite-plugin-pwa.netlify.app/guide/generate.html#generate-service-worker
+  return VitePWA({
+    manifest: getManifest({ manifestJson: appConfig.manifestJson }),
+    strategies: 'generateSW',
+    workbox: getWorkbox(),
+  })
 }
