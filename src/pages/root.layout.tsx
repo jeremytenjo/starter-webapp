af9fbc6d3776b1406b2e3f817aa7ca94a1f8ac60
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-25 09:31:48 -0500

    removed udpatesw prompt

--- src/pages/root.layout.tsx
+++ src/pages/root.layout.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import { SnackbarProvider } from '../lib/components/Snackbar/Snackbar'
-import ReloadPrompt from '../lib/components/workbox/ReloadPrompt/ReloadPrompt'
 
 export default function ShellLayout({ children }) {
   return (
     <>
       <SnackbarProvider>
         <main>{children}</main>
-        <ReloadPrompt />
       </SnackbarProvider>
     </>
   )
