9770ac657119228a070a4f4d4433ad3396832f7e
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-27 14:45:17 -0500

    org

--- devtools/utils/node/createFolder.ts
+++ devtools/utils/node/createFolder.ts
@@ -4,6 +4,7 @@ type Props = {
   paths: string[]
 }
 
+// https://github.com/jprichardson/node-fs-extra/blob/master/docs/ensureDir-sync.md
 export default async function createFolder({ paths }: Props) {
   await Promise.all(
     paths.map(async (path) => {
