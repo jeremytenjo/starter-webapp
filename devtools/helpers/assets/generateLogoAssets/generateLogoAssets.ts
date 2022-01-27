7625fd684afc610ba728047b41589485c718e522
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-27 14:24:16 -0500

    org

--- devtools/helpers/assets/generateLogoAssets/generateLogoAssets.ts
+++ devtools/helpers/assets/generateLogoAssets/generateLogoAssets.ts
@@ -9,18 +9,18 @@ import type { Props as ConvertImageProps } from '../../../utils/images/convertIm
 export default async function generateLogoAssets() {
   const rootPath = path.join(process.cwd(), 'public', 'images', 'logo')
   const logoPath = path.join(rootPath, 'logo.svg')
-  const folderOutputPath = path.join(rootPath, 'assets')
-  const applePath = path.join(folderOutputPath, 'apple')
-  const sizesPath = path.join(folderOutputPath, 'sizes')
-  const pngLogoPath = path.join(folderOutputPath, 'logo.png')
+  const outputFolderPath = path.join(rootPath, 'assets')
+  const applePath = path.join(outputFolderPath, 'apple')
+  const sizesPath = path.join(outputFolderPath, 'sizes')
+  const pngLogoPath = path.join(outputFolderPath, 'logo.png')
 
   const getSizeIconName = (name: string) => path.join(sizesPath, name)
   const getAppleIconName = (name: string) => path.join(applePath, name)
 
   const iconList = [
     {
-      filePath: logoPath,
       outputPath: pngLogoPath,
+      filePath: logoPath,
       format: 'png',
       width: 512,
       height: 512,
