72766d1e4736e5173891e035c2507b720cf1ea23
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-27 11:49:39 -0500

    comment

--- devtools/storybook/main.js
+++ devtools/storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // https://storybook.js.org/docs/react/configure/overview#configure-your-storybook-project
   stories: ['../../src/**/sb.stories.tsx', '../../functions/src/**/sb.stories.tsx'],
   staticDirs: ['../../public'],
   addons: [
