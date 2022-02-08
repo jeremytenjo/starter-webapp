874c8e06a4c8b67b6b89d85056ea53a5d695e753
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-08 08:56:58 -0500

    c

--- devtools/codeGenerator/templates/function.cjs
+++ devtools/codeGenerator/templates/function.cjs
@@ -1,9 +1,14 @@
+// https://github.com/jeremytenjo/quick-component-creator/tree/master#component-type-properties
 const files = [
   {
     path: ({ name }) => `${name}.ts`,
-    template: ({ name }) => `type Props = {name}
+    template: ({ name, helpers }) => `type ${helpers.changeCase.capitalCase(
+      name,
+    )}Props = {name: string}
     
-    export default function ${name}({name}: Props) {}`,
+    export default function ${name}({name}: ${helpers.changeCase.capitalCase(
+      name,
+    )}Props) {}`,
   },
 ]
 
