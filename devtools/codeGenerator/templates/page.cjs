e44960c1eec0761b99e7124e802583797b35c834
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-03-16 20:05:37 -0400

    page

--- devtools/codeGenerator/templates/page.cjs
+++ devtools/codeGenerator/templates/page.cjs
@@ -8,18 +8,12 @@ const files = [
   },
   {
     path: () => 'routes.tsx',
-    template: ({ name }) => `import React, { lazy } from 'react'
-    import { Routes, Route } from 'react-router-dom'
-    
-    const ${name} = lazy(() => import('./'))
+    template: ({ name }) => `import React from 'react'
 
-    export default function ${name}Routes() {
-      return (
-        <Routes>
-          <Route path='/' element={<${name} />} />
-        </Routes>
-      )
-    }`,
+    export default [
+      { path: '/', element: () => import('./${name}').then((res) => <res.default />) },
+    ]
+    `,
   },
 ]
 
