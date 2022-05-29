ffb47a79a0cc1e47fdc03a3237498fc463363a78
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-29 08:31:14 -0400

    shell

--- devtools/ladle/scripts/buildLadle/buildLadle.ts
+++ devtools/ladle/scripts/buildLadle/buildLadle.ts
@@ -1,4 +1,4 @@
-import shell from '../../../utils/node/shell.js'
+import shell from '../../../utils/node/shell.cjs'
 
 export default function buildLadle() {
   shell('ladle build --config ./devtools/ladle')
