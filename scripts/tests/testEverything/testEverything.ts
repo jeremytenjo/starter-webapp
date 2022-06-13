f4d4b847929d5278b6b89d96434d418a4b194f35
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-13 18:56:47 -0400

    comm

--- scripts/tests/testEverything/testEverything.ts
+++ scripts/tests/testEverything/testEverything.ts
@@ -5,7 +5,7 @@ export default async function testEverything() {
     await shell('npm run tests:eslint')
     await shell('npm run tests:ts-types')
     await shell('npm run tests:unit:ci')
-    await shell('npm run tests:e2e:run:all')
+    // await shell('npm run tests:e2e:run:all')
   } catch (error: any) {
     throw new Error(error)
   }
