6277cd719717f34dab609d0d6a0eb4376d6afabb
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-13 19:07:03 -0400

    window.location.host

--- devtools/vite/plugins/html/items/serviceWorker.ts
+++ devtools/vite/plugins/html/items/serviceWorker.ts
@@ -2,7 +2,7 @@ export default function registerServiceWorker({ isProdMode }) {
   return isProdMode
     ? `
   <script>
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && !window.location.host.includes('localhost:')) {
       if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
           navigator.serviceWorker.register('/sw.js', { scope: '/' })
