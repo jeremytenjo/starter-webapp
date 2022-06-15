b29781c0e451795199eb83091deb1dbf5a552acd
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-15 10:41:41 -0400

    usewbui: get types

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '@useweb/ui/Button'
 
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button name='hello name'>Hello button</Button>
     </div>
   )
 }
