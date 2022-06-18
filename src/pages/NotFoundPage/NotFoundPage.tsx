9b92c53caddc3cf3a24b1b191af3a7447b4a42eb
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-18 15:57:08 -0400

    build

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-
-import Text from '../../lib/components/Text/Text'
+import Text from '@useweb/ui/Text'
 
 export default function NotFoundPage() {
   return (
