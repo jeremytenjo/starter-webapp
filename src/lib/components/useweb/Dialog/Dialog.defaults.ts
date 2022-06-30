cf29e0379ff45c89251d5ac9a559a7ed1d21c8dc
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-30 15:48:12 -0400

    pretty

--- src/lib/components/useweb/Dialog/Dialog.defaults.ts
+++ src/lib/components/useweb/Dialog/Dialog.defaults.ts
@@ -1,20 +1,20 @@
 // https://mui.com/customization/theme-components/#global-style-overrides
 // import in src/theme/mui/MuiProvider.jsx
-import { type ComponentDefaultsProps } from "@useweb/ui-theme";
+import { type ComponentDefaultsProps } from '@useweb/ui-theme'
 
 const defaults: ComponentDefaultsProps = {
   styleOverrides: {
     root: {
-      "& .MuiPaper-root": {
-        backgroundColor: "transparent",
-        color: "white",
+      '& .MuiPaper-root': {
+        backgroundColor: 'transparent',
+        color: 'white',
       },
-      "& .MuiDialog-scrollPaper": {
-        background: "rgb(255 255 255 / 33%)",
-        backdropFilter: "blur(7px) saturate(150%)",
+      '& .MuiDialog-scrollPaper': {
+        background: 'rgb(255 255 255 / 33%)',
+        backdropFilter: 'blur(7px) saturate(150%)',
       },
     },
   },
-};
+}
 
-export default defaults;
+export default defaults
