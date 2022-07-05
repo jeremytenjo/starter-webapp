d84b804fc39f20ab12d2e3e4e087192ba65a0261
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-05 11:18:00 -0400

    story

--- devtools/codeGenerator/templates/story.cjs
+++ devtools/codeGenerator/templates/story.cjs
@@ -1,7 +1,28 @@
 const docsTemplate = () => {
   return {
     path: ({ name }) => `stories/${name}.docs.mdx`,
-    template: ({ name }) => `# ${name}`,
+    template: ({ name, helpers }) => {
+      return `import React from 'react'
+      import {
+        Title,
+        Description,
+        Primary,
+        ArgsTable,
+        PRIMARY_STORY,
+      } from '@storybook/addon-docs'
+      
+      // https://storybook.js.org/docs/react/writing-docs/docs-page#remixing-docspage-using-doc-blocks
+      export default function Docs() {
+        return (
+          <>
+            <Title />
+            <Description>This is a ${helpers.changeCase.pascalCase(name)}</Description>
+            <Primary />
+            <ArgsTable story={PRIMARY_STORY} />
+          </>
+        )
+      }`
+    },
   }
 }
 
